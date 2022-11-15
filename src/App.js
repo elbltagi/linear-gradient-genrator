8e540c59fcc05dea0d9c53ef808ae529e3bf13a1
unknown <youssefsalah469@gmail.com>
2022-11-15 22:44:55 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [range1,setRange1] = useState(0);
+  const [range2,setRange2] = useState(1);
+  const [firstColor,setFirstColor] = useState("#fff");
+  const [secColor,setSecColor] = useState("#000");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{flexDirection:"column",gap:"20px",background:`linear-gradient(${range1}deg,${firstColor},${secColor})`}}>
+      <div className='glass-effect'>
+        <h1 style={{textAlign:"center"}}>Gradient genrator</h1>
+        <h1 style={{color:"rgb(0,205,255)"}}>background: {`linear-gradient(${range1}deg,${firstColor},${secColor})`}; </h1>
+      </div>
+      <div style={{width:"50%",display:"flex",alignItems:"center",justifyContent:"space-evenly",}}>
+        <input type={"color"} color={firstColor} onChange={(e)=>setFirstColor(e.target.value)}/>
+        <input type={"number"} value={range1} onChange={e=>setRange1(e.target.value)}/>
+        <input type={"color"} color={secColor} onChange={(e)=>setSecColor(e.target.value)}/>
+      </div>
     </div>
   );
 }
